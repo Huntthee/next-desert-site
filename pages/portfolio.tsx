8361e133a9fca6835792d935c3dd3e0f067a677a
Huntthee <mikeallison000@gmail.com>
2022-01-25 12:09:10 -0700

    setting up file structure

---
+++ pages/portfolio.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function Portfolio() {
+  return (
+    <>
+      <h1>Portfolio</h1>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </>
+  ) 
+}
---
+++ pages/waitlist.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Waitlist() {
+  return (
+    <>
+      <h1>Waitlist</h1>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </>
+  );
+}
