870e9856d521f51d0c984334075a50f5c7458c72
Huntthee <mikeallison000@gmail.com>
2022-01-25 14:28:01 -0700

    converted commissions content, copied over basic global styles, moved home content into seperate component

---
+++ pages/components/home.tsx
@@ -0,0 +1,69 @@
+import Link from "next/link";
+
+export default function HomeContent() {
+  return (
+    <>
+      <section>
+        <h1>DesertWhale</h1>
+        <div>
+          <p>Commission Status:</p>
+        </div>
+
+        <div>
+          <Link href="/portfolio">
+            <a>View My Work</a>
+          </Link>
+          <Link href="/commissions">
+            <a>Hire Me!</a>
+          </Link>
+        </div>
+
+        <div>
+          <a
+            href="https://twitter.com/TheDesertWhale"
+            target="_blank"
+            title="Twitter"
+            rel="noreferrer"
+          >
+            <i className="fab fa-twitter"></i>
+          </a>
+          <a
+            href="https://www.twitch.tv/thedesertwhale"
+            target="_blank"
+            title="Twitch"
+            rel="noreferrer"
+          >
+            <i className="fab fa-twitch"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/dessertwhale/"
+            target="_blank"
+            title="Instagram"
+            rel="noreferrer"
+          >
+            <i className="fab fa-instagram"></i>
+          </a>
+          <a
+            href="https://www.tiktok.com/@the_desertwhale?lang=en"
+            target="_blank"
+            title="TikTok"
+            rel="noreferrer"
+          >
+            <i className="fab fa-tiktok"></i>
+          </a>
+          <a
+            href="https://desertwhale.threadless.com/"
+            target="blank"
+            title="Threadless"
+          >
+            <i className="fas fa-tshirt"></i>
+          </a>
+        </div>
+        <div>
+          <p>More about me!</p>
+          <i className="fas fa-arrow-down"></i>
+        </div>
+      </section>
+    </>
+  ); 
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import Navigation from './components/navigation'
+import HomeContent from './components/home'
 
 const Home: NextPage = () => {
   return (
@@ -23,71 +24,11 @@ const Home: NextPage = () => {
         ></link>
       </Head>
 
-      <main>
+      <main className={styles.homepage}>
         <nav>
           <Navigation></Navigation>
         </nav>
-        <section>
-          <h1>DesertWhale</h1>
-          <div>
-            <p>Commission Status:</p>
-          </div>
-
-          <div>
-            <Link href="/portfolio">
-              <a>View My Work</a>
-            </Link>
-            <Link href="/commissions">
-              <a>Hire Me!</a>
-            </Link>
-          </div>
-
-          <div>
-            <a
-              href="https://twitter.com/TheDesertWhale"
-              target="_blank"
-              title="Twitter"
-              rel="noreferrer"
-            >
-              <i className="fab fa-twitter"></i>
-            </a>
-            <a
-              href="https://www.twitch.tv/thedesertwhale"
-              target="_blank"
-              title="Twitch"
-              rel="noreferrer"
-            >
-              <i className="fab fa-twitch"></i>
-            </a>
-            <a
-              href="https://www.instagram.com/dessertwhale/"
-              target="_blank"
-              title="Instagram"
-              rel="noreferrer"
-            >
-              <i className="fab fa-instagram"></i>
-            </a>
-            <a
-              href="https://www.tiktok.com/@the_desertwhale?lang=en"
-              target="_blank"
-              title="TikTok"
-              rel="noreferrer"
-            >
-              <i className="fab fa-tiktok"></i>
-            </a>
-            <a
-              href="https://desertwhale.threadless.com/"
-              target="blank"
-              title="Threadless"
-            >
-              <i className="fas fa-tshirt"></i>
-            </a>
-          </div>
-          <div>
-            <p>More about me!</p>
-            <i className="fas fa-arrow-down"></i>
-          </div>
-        </section>
+        <HomeContent></HomeContent>
       </main>
 
       <footer>&copy; TheDesertWhale 2022</footer>
