6a990939dd13d3f365c03cdb2c30f63ee95f911f
Huntthee <mikeallison000@gmail.com>
2022-01-31 13:09:56 -0700

    script for generating portfolio content transferred & converted

--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import Footer from './components/footer';
 import Navigation from './components/navigation'
+import Script from 'next/script';
 
 export default function Portfolio() {
   return (
@@ -9,10 +10,19 @@ export default function Portfolio() {
         <Navigation></Navigation>
       </nav>
       <h1>Portfolio</h1>
+      <section id="portfolio-gallery"></section>
       <Link href="/">
         <a>Home</a>
       </Link>
       <Footer></Footer>
+      <Script
+        id="scripts"
+        src="/scripts/portfolio.js"
+        strategy="lazyOnload"
+        onError={(e) => {
+          console.error("Script failed to load", e);
+        }}
+      />
     </>
   ); 
 }
---
+++ public/scripts/portfolio.js
@@ -0,0 +1,308 @@
+const gallery = [
+  {
+    alt: "Assorted Sombreros and a cowboy hat for SmileyRaider96",
+    src: '"images/smiley-hats.jpg"',
+    desc: "Sub Badges for SmileyRaider96",
+  },
+  {
+    alt: "Neon Alien themed sub badges for Millz_XO on Twitch",
+    src: '"images/millz-sub-badges.jpg"',
+    desc: "Sub Badges for MILLZ_xo",
+  },
+  {
+    alt: "Inside joke turned emote for NoAimDontFlame",
+    src: '"images/noaim-kiss.jpg"',
+    desc: "Emote for NoAimDontFlame",
+  },
+  {
+    alt: "Boba-fett drinking Boba Tea emote for FranklinTurtz",
+    src: '"images/franklin-boba.jpg"',
+    desc: "Emote for FranklinTurtz",
+  },
+  {
+    alt: "Gengar themed emotes for AceAttorneyMEEKS",
+    src: '"images/meeks-emotes.jpg"',
+    desc: "Emotes for AceAttorneyMEEKs",
+  },
+  {
+    alt: "Second batch of orc themed emotes for Notorious_BLT on Twitch",
+    src: '"images/notor-emote-set-2.jpg"',
+    desc: "More Emotes for Notorious_BLT!",
+  },
+  {
+    alt: "A collection of Emotes for Kamizard",
+    src: '"images/kami-emotes.jpg"',
+    desc: "Emotes for Kamizard",
+  },
+  {
+    alt: "Starting scene of Gengar and Blastoise battling.",
+    src: '"images/mrb-background.webp"',
+    desc: "Illustrated background image for MrBrightside",
+  },
+  {
+    alt: "An Emote of Melvin's dog Stella",
+    src: '"images/stella-for-melvin.jpg"',
+    desc: "Emote for Melvin",
+  },
+  {
+    alt: "Cartoon themed t-rex skeleton logo",
+    src: '"images/tbone-logo.jpg"',
+    desc: "Logo for Tb0neT0ne",
+  },
+  {
+    alt: "Polar bear themed emotes and logo for Omar94 on Twitch",
+    src: '"images/omar-emotes-logo.jpg"',
+    desc: "Emotes & Logo for Omar94",
+  },
+  {
+    alt: "Starting scene of Gengar and Blastoise battling.",
+    src: '"images/meeks-starting-soon.jpg"',
+    desc: "Starting Soon scene for AceAttorneyMEEKs",
+  },
+  {
+    alt: "Dinosaur bone themed Twitch Scenes for Tb0neT0ne",
+    src: '"images/tbone-panel.webp"',
+    desc: "Scenes for Tb0neT0ne, now on YouTube!",
+  },
+  {
+    alt: "Various muffin styled Sub badges for Tzenglishmuffin",
+    src: '"images/muffin-panel.webp"',
+    desc: "Sub Badges for Tzenglishmuffin",
+  },
+  {
+    alt: "Emotes and Merch goods of OG's mascot for OG_Monrk",
+    src: '"images/monrk-panel.webp"',
+    desc: "OG_Monrk's Emotes and Merch",
+  },
+  {
+    alt: "A wonderful spread of artwork for DillThrills",
+    src: '"images/thrills-panel.webp"',
+    desc: "DillThrill's Emote, Logo, Panels & Merch",
+  },
+  {
+    alt: "An Emote of EllyPypa's face for EllyPypa on Twitch",
+    src: '"images/ellypypa-emote.webp"',
+    desc: "Emote for EllyPypa on Twitch",
+  },
+  {
+    alt: "A YouTube Thumbnail for Notorious_BLT's Dark Souls videos",
+    src: '"images/notor-yt.webp"',
+    desc: "YouTube Thumbnail for Notorious_BLT",
+  },
+  {
+    alt: "A Koala avatar for PrincessPlaya",
+    src: '"images/princess-koala.webp"',
+    desc: "PrincessPlaya's Koala Avatar",
+  },
+  {
+    alt: "The Unbound Icon on WorldAnvil",
+    src: '"images/unbound-icon.webp"',
+    desc: "Icon for Unbound on World Anvil",
+  },
+  {
+    alt: "YouTube Thumbnails for SilverKestrelStudios",
+    src: '"images/Tales_Of_Doroda_01.webp"',
+    desc: "YouTube Thumbnails for SilverKestrelStudios",
+  },
+  {
+    alt: "YouTube Thumbnails for SilverKestrelStudios",
+    src: '"images/KestrelQuests_01.webp"',
+    desc: "YouTube Thumbnails for SilverKestrelStudios",
+  },
+  {
+    alt: "OG_Monrk's Illustrated Avatar for Merchandise, featuring custom character",
+    src: '"images/SOM_Final.webp"',
+    desc: "OG_Monrk's Illustrated Avatar for Merchandise",
+  },
+  {
+    alt: "Illustrated character avatar for meekstv featuring the Pokemon, Gengar",
+    src: '"images/meeks-avatar.webp"',
+    desc: "Meeks Illustrated Avatar",
+  },
+  {
+    alt: "Large panel of various emotes for various clients",
+    src: '"images/Emote-Spread.webp"',
+    desc: "A wonderful variety of styles to suit your personality",
+  },
+  {
+    alt: "Starting scene depicting Arcadio and Ghostface, and emotes for Arcadio",
+    src: '"images/Arcadio-Scene-and-Emotes.webp"',
+    desc: "Starting Scene and Emotes for Arcadio",
+  },
+  {
+    alt: "A snappy advertisement for Kestrel Quests DnD sessions",
+    src: '"images/DND-TIME-Advert.webp"',
+    desc: "Advertisement for Kestrel Quests DnD",
+  },
+  {
+    alt: "Personalized Emotes for JobberJosh",
+    src: '"images/JobberJosh-Emotes.webp"',
+    desc: "Emote Set for JobberJosh",
+  },
+  {
+    alt: "Emote Set of goblins and characturized version of 'Notor' for Notorious_BLT",
+    src: '"images/Notorious-BLT-Emotes-Set.webp"',
+    desc: "Emote Set for Notorious_BLT",
+  },
+  {
+    alt: "panel of Sub Badges and an Illustrated Scene depicting Rorlann, for Rorlann",
+    src: '"images/Rorlann-Sub-Badges-and-Scene.webp"',
+    desc: "Sub Badges and Illustrated Scene for Rorlann",
+  },
+  {
+    alt: "Complete floral scene setup for Niladien's Twitch channel",
+    src: '"images/Twitch-set-for-Niladien.webp"',
+    desc: "Twitch Scenes and Panels for Niladien",
+  },
+  {
+    alt: "Channel banner and panels for SuperStooob on Twitch",
+    src: '"images/SuperStooob-Banners-and-panels.webp"',
+    desc: "Banners and Panels for SuperStoob",
+  },
+  {
+    alt: "Set of blue and yellow scenes for ThePluckyAvatar on Twitch",
+    src: '"images/ThePluckyAvatar-Scene-Set.webp"',
+    desc: "Twitch Scenes for ThePluckyAvatar",
+  },
+  {
+    alt: "Set of four emotes for YoloSoloFolo on Twitch",
+    src: '"images/YoloSolofolo-Emotes.webp"',
+    desc: "Emotes for YoloSoloFolo",
+  },
+  {
+    alt: "Set of 12 Owl emotes for SpondeeBob on Twitch",
+    src: '"images/Spondeebob-Emotes-2.webp"',
+    desc: "Emotes for SpondeeBob",
+  },
+  {
+    alt: "Set of Emotes, Banners and Badges with firefighter theme for Smokeeater on Twitch",
+    src: '"images/Smokeeater-Set.webp"',
+    desc: "Emotes, Badges and Banners for Smoke_eater3696",
+  },
+  {
+    alt: "Set of Panels, Emote and Sub Badges for SilverKestrelStudios on Twitch",
+    src: '"images/SilverKestrelStudios-Panels-emote-and-sub-badges.webp"',
+    desc: "Panels, Emote and Sub Badges for SilverKestrelStudios",
+  },
+  {
+    alt: "Personal Panels for TheDesertWhale on Twitch",
+    src: '"images/Sample-Set-Twitch-Panels.webp"',
+    desc: "Set of Panels for Myself",
+  },
+  {
+    alt: "Set of 3 emotes for Prxmeth3us on Twitch",
+    src: '"images/Prxm3theus-emote-set.webp"',
+    desc: "Emotes for Prxmeth3us",
+  },
+  {
+    alt: "Set of custom Illustrated Panels for Notorious_BLT on Twitch",
+    src: '"images/Notorious_BLT-Illustrated-Panels.webp"',
+    desc: "Illustrated Panels for Notorious_BLT",
+  },
+  {
+    alt: "Various logs on fire for NoaimDontFlame's sub badges",
+    src: '"images/NoaimDontFlame-Sub-Badges.webp"',
+    desc: "Sub Badges for NoAimDontFlame",
+  },
+  {
+    alt: "Set of Scenes and Panels for MrzMami on Twitch",
+    src: '"images/MrzMami-Basic-Scenes-Package-with-Banners.webp"',
+    desc: "Scenes and Panels for MrzMami",
+  },
+  {
+    alt: "Stream schedule for Mr Brightside",
+    src: '"images/MR-Brightside-Schedule.webp"',
+    desc: "Stream Schedule for Mr Brightside",
+  },
+  {
+    alt: "Set of 4 Emotes for Kamizard on Twitch",
+    src: '"images/Kamizard-Emotes.webp"',
+    desc: "Emotes for Kamizard",
+  },
+  {
+    alt: "Set of 4 Emotes for JoelTheShortOne on Twitch",
+    src: '"images/Joeltheshortone-Emotes.webp"',
+    desc: "Emotes for JoelTheShortOne",
+  },
+  {
+    alt: "Set of 5 Emotes for JoeAnnaB on Twitch",
+    src: '"images/Joanna-Emotes.webp"',
+    desc: "Emotes for JoeAnnaB",
+  },
+  {
+    alt: "Package of Stream Overlays for Mr Brightside on Twitch",
+    src: '"images/Mr-Brightside-Overlays-set.webp"',
+    desc: "Overlays for Mr Brightside",
+  },
+  {
+    alt: "Set of 4 Emotes for Jayber on Twitch",
+    src: '"images/Jayber-emotes.webp"',
+    desc: "Emotes for Jayber",
+  },
+  {
+    alt: "Stream Schedule for HighheeledGamer on Twitch",
+    src: '"images/HighheeledGamer-Schedule.webp"',
+    desc: "Stream Schedule for HighheeledGamer",
+  },
+  {
+    alt: "Advertisement for Heycallmeclay playing Cookout: A Sandwich Tale",
+    src: '"images/Heycallmeclay-Cookout-Advert.webp"',
+    desc: "Advertisment for Heycallmeclay",
+  },
+  {
+    alt: "Discount Set of Fruit Sub Badges for new streamers",
+    src: '"images/Fruit-Sub-Badges.webp"',
+    desc: "Placeholder Fruit Sub Badges",
+  },
+  {
+    alt: "Animated background for EvolMelky",
+    src: '"images/EvolMelky-Animated-BG.webp"',
+    desc: "Background for EvolMelky",
+  },
+  {
+    alt: "Set of 6 shark emotes for DumpsterShark on Twitch",
+    src: '"images/DumpsterShark-Emotes.webp"',
+    desc: "Emotes for DumpsterShark",
+  },
+  {
+    alt: "Personal Stream Schedule",
+    src: '"images/des-schedule.webp"',
+    desc: "Stream Schedule for Myself",
+  },
+  {
+    alt: "Illustrated starting scene for CadesArcade featuring monkey mascot in arcade.",
+    src: '"images/CadesArcade-Illustrated-Scene.webp"',
+    desc: "Illustrated Scene for CadesArcade",
+  },
+  {
+    alt: "Emote and Sub Badges for BabyCow on Twitch",
+    src: '"images/BabyCow-Emote-and-Badges.webp"',
+    desc: "Emote and Sub Badges for BabyCow",
+  },
+  {
+    alt: "Stream Schedule for Arcadio on Twitch",
+    src: '"images/Arcadio-Schedule.webp"',
+    desc: "Stream Schedule for Arcadio",
+  },
+  {
+    alt: "Stream Schedule for Cades Arcade on Twitch",
+    src: '"images/CA-SCHEDULE.webp"',
+    desc: "Stream Schedule for Cades Arcade",
+  },
+];
+
+//loop for iterating over images and creating the portfolio gallery
+let output = '<div id="image-container">';
+
+const images = gallery.forEach(function(item){
+  output += `
+  <div class="gallery-image">
+    <h3>${item.desc}</h3>
+    <img src=${item.src} alt=${item.alt} onContextMenu="return false;" loading="lazy">
+  </div>
+  `;
+});
+
+output += '</div>';
+
+document.getElementById('portfolio-gallery').innerHTML = output;
