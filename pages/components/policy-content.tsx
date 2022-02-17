24141caa3b27bcda53c40b2f99ad419e58cb9942
Huntthee <mikeallison000@gmail.com>
2022-02-17 12:26:49 -0700

    Desktop styles applied to Policy page

--- pages/components/policy-content.tsx
+++ pages/components/policy-content.tsx
@@ -1,7 +1,7 @@
 export default function PolicyContent() {
   return (
     <>
-      <section className="m-2 lg:m-7">
+      <section className="m-2 lg:m-7 xl:mx-9">
         <h1 className="text-center text-2xl font-bold mt-5">
           Policy and Terms of Service
         </h1>
@@ -14,27 +14,27 @@ export default function PolicyContent() {
         </h3>
 
         <ul className="bg-shade p-4 list-disc rounded mb-5">
-          <li className="bg-salmon text-center py-3 my-3 list-none">
+          <li className="bg-salmon text-center py-3 my-3 list-none xl:mx-9">
             Payment accepted through PayPal <strong>ONLY</strong>.
           </li>
-          <li>I work Monday to Friday</li>
-          <li>Email as the primary form of contact unless told otherwise.</li>
-          <li>
+          <li className="xl:ml-4 xl:mt-7">I work Monday to Friday</li>
+          <li className="xl:ml-4">Email as the primary form of contact unless told otherwise.</li>
+          <li className="xl:ml-4">
             Please do not ask me to copy or replicate another artists style or
             image.
           </li>
-          <li>
+          <li className="xl:ml-4">
             ALL files will be PNG, sized according to the commission
             requirements.
           </li>
-          <li>
+          <li className="xl:ml-4">
             I do not do vector illustrations for regular commissions. If you
             require a higher resolution image, different sizes, resolutions,
             prefer RGB vs CMYK, or intend to use it for merchandising please let
             me know before I start, as this will be considered a special request
             and may come with additional charges.
           </li>
-          <li>
+          <li className="xl:ml-4">
             The Client MUST provide as much information and visual reference as
             possible, if I am required to do research, you will be charged
             hourly for me having to do so. If I am expected to come up with an
@@ -42,7 +42,7 @@ export default function PolicyContent() {
             additional charge for Character Design will be added on top of the
             original commission fee.
           </li>
-          <li>
+          <li className="xl:ml-4">
             Two revisions are included for every piece of work, one after the
             preliminary design and one at the final stages of design. Any
             additional, excessive changes will be charged by the hour @ $20/hr.
@@ -50,7 +50,7 @@ export default function PolicyContent() {
             do not save or upload them to social media.
           </li>
 
-          <li>
+          <li className="xl:ml-4">
             I work on My time, it ensures the best results. If you want me to
             work on your time, rush a commission or move your project to the
             front of the line I require 100% payment upfront. Rush Orders adds
@@ -58,21 +58,21 @@ export default function PolicyContent() {
             will be treated as a nonrefundable deposit and ensure your spot in
             line. Work will only start once payment has been received.
           </li>
-          <li>
+          <li className="xl:ml-4">
             I ask that you credit me appropriately for the final product
             when/where such credit may be reasonably provided. I must also be
             allowed to display the work in a portfolio or on social media for
             personal marketing purposes at an agreed time after the completion
             of the project.
           </li>
-          <li>
+          <li className="xl:ml-4">
             I reserve all rights of reproduction and all copyrights to the
             commission, the preliminary design, and any incidental work made in
             the creation of the commission. You can purchase the rights to the
             commission at an added cost that can be discussed during the
             commission process.
           </li>
-          <li>
+          <li className="xl:ml-4">
             I am not responsible for the content on your website or pages, or
             for how you choose to upload the images once I have finished them.
           </li>
@@ -80,13 +80,13 @@ export default function PolicyContent() {
 
         <h3 className="font-bold mb-2">Client Is Allowed To:</h3>
         <ul className="bg-shade p-4 list-disc rounded mb-5">
-          <li>Print and use the commissioned piece for personal use.</li>
-          <li>Make stickers out of the commission for personal use.</li>
-          <li>
+          <li className="xl:ml-4">Print and use the commissioned piece for personal use.</li>
+          <li className="xl:ml-4">Make stickers out of the commission for personal use.</li>
+          <li className="xl:ml-4">
             Claim the rights to their character/s in the piece, not the piece
             itself.
           </li>
-          <li>
+          <li className="xl:ml-4">
             Post the commissioned piece on social media, use it as a display
             photo on any platform.
           </li>
@@ -94,22 +94,22 @@ export default function PolicyContent() {
 
         <h3 className="font-bold mb-2">The Client Is Not Allowed To:</h3>
         <ul className="bg-shade p-4 list-disc rounded mb-5">
-          <li>
+          <li className="xl:ml-4">
             You may not use personal commissions commercially for financial
             gain. If you would like to commission a commercial piece, it must be
             discussed in the initial ordering phase.
           </li>
-          <li>
+          <li className="xl:ml-4">
             Intentionally destroy, alter, modify, or change the piece in any way
             without permission. Please contact me for any additional changes or
             modifications.
           </li>
-          <li>Claim the artwork as their own or someone elses.</li>
+          <li className="xl:ml-4">Claim the artwork as their own or someone elses.</li>
         </ul>
 
         <h3 className="font-bold mb-2">Refunds, Cancellations, Ownership & Copyright.</h3>
-        <ul className="bg-shade p-4 list-disc rounded mb-5">
-          <li>
+        <ul className="bg-shade p-4 list-disc rounded mb-5 xl:mb-8">
+          <li className="xl:ml-4">
             Full or even partial refunds are not available if the client is
             unhappy with the finished product. We go over everything during 2
             revision processes, all issues should be resolved before the
@@ -117,26 +117,26 @@ export default function PolicyContent() {
             finished piece. All additional revisions will be charged hourly, @
             $20/hr.
           </li>
-          <li>
+          <li className="xl:ml-4">
             I, the artist, reserve rights to terminate the agreement in the
             event the Client is more than 30 days late in payment.
           </li>
-          <li>
+          <li className="xl:ml-4">
             I reserve the rights to move onto other work/projects and the Client
             may lose their place in line, if they take more than 3 days to
             respond to questions/messages regarding the commission.
           </li>
-          <li>
+          <li className="xl:ml-4">
             Title of the commission remains with me until I am paid in full.
           </li>
-          <li>
+          <li className="xl:ml-4">
             Again, I do not offer vector illustrations for regular commissions.
             Please be fully aware of that fact before filing for a refund. If
             you have not specified to me the file format or other details before
             I have started the commission process, refunds will not even be
             considered.
           </li>
-          <li>
+          <li className="xl:ml-4">
             If the contract is terminated for whatever reason, the rights to the
             commission remain with me, and the work can be sold to another
             customer.
