b48aeb45778b24d8400d4a03887c83853efafe32
Huntthee <mikeallison000@gmail.com>
2022-03-17 11:57:30 -0600

    styles applied, polishing fonts

--- pages/waitlist.tsx
+++ pages/waitlist.tsx
@@ -15,35 +15,34 @@ export default function Waitlist() {
         <Navigation></Navigation>
       </nav>
       <h1 className="text-2xl text-center font-bold mt-5">Waitlist</h1>
-        <section className="flex justify-around my-5 py-4 bg-lorange">
-          <ul>
-            <h3>Waiting</h3>
-            {waiting.map(client => (
-              <li>{client.name}</li>
+        <section className="flex justify-around my-5 py-4 bg-lorange w-3/4 m-auto">
+          <ul className="bg-salmon p-3 rounded">
+            <h3 className="font-primary text-xl text-center pb-3">Waiting</h3>
+            {waiting.map((client, index) => (
+              <li key={index} className="bg-white rounded-full p-1 text-center m-1 font-special text-xl">{client.name}</li>
             ))}
           </ul>
-          <ul>
-            <h3>Working</h3>
-            {working.map(client => (
-              <li>{client.name}</li>
+          <ul className="bg-salmon p-3 rounded">
+            <h3 className="font-primary text-xl text-center pb-3">Working</h3>
+            {working.map((client, index) => (
+              <li key={index} className="bg-white rounded-full p-1 text-center m-1 font-special text-xl">{client.name}</li>
             ))}
           </ul>
-          <ul>
-            <h3>On Hold</h3>
-            {onHold.map(client => (
-              <li>{client.name}</li>
-            ))}
-          </ul>
-        </section>
-        <section>
-          <ul>
-            <h3>Complete</h3>
-            {complete.map(client => (
-              <li>{client.name} - {client.work}</li>
+          <ul className="bg-salmon p-3 rounded">
+            <h3 className="font-primary text-xl text-center pb-3">On Hold</h3>
+            {onHold.map((client, index) => (
+              <li key={index} className="bg-white rounded-full p-1 text-center m-1 font-special text-xl">{client.name}</li>
             ))}
           </ul>
         </section>
-      <p className="text-center my-5">If content fails to load, refresh the page.</p>
+        <section className="w-2/3 m-auto mb-7">
+          <ul className="bg-salmon p-3 rounded max-h-10 overflow-y-scroll">
+            <h3 className="font-primary text-xl text-center pb-3">Complete</h3>
+            {complete.map((client, index) => (
+              <li key={index} className="bg-white rounded-full p-1 text-center m-1 font-special text-xl mx-8">{client.name} - {client.work}</li>
+            ))}
+          </ul>
+        </section>
       <nav>
         <Navigation></Navigation>
       </nav>
