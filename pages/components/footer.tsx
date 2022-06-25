a8b5372237b7868ab1da71e0ada7b0de345c1e9e
Huntthee <mikeallison000@gmail.com>
2022-06-25 08:25:57 -0600

    Added attribute link to footer

--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -3,6 +3,7 @@ export default function Footer() {
     <>
       <footer className="mt-7 p-2">
         <p>&copy; TheDesertWhale 2022</p>
+        <a href="https://huntthee.github.io" target="_blank">Website built by Michael Allison</a>
       </footer>
     </>
   );
