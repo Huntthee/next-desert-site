745169069d86164f23ca76a718e4e9ded2871ed4
Huntthee <mikeallison000@gmail.com>
2022-06-25 08:22:59 -0600

    Updated waitlist

--- public/scripts/waitlist.js
+++ public/scripts/waitlist.js
@@ -3,7 +3,22 @@ const waiting = [
     name: "Arcadio",
   },
   {
-    name: "SquareTheBear1"
+    name: "SquareTheBear1",
+  },
+  {
+    name: "Smezz0",
+  },
+  {
+    name: "MrBrightside",
+  },
+  {
+    name: "Kamizard",
+  },
+  {
+    name: "BumbleMage",
+  },
+  {
+    name: "Chester Dwight",
   },
 ];
 
