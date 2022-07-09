a17a65f6c5a4d78e744ddf9007c91bccec195517
Huntthee <mikeallison000@gmail.com>
2022-07-09 09:27:40 -0600

    price fixes and adjustments done

--- pages/components/commissions-content.tsx
+++ pages/components/commissions-content.tsx
@@ -1,7 +1,31 @@
 export default function CommissionContent() {
   return (
     <>
-      <p className="text-xl">Take a look at some of the packages on offer, and be sure to read the policy if you're seeking a commission.</p>
+      <p className="text-xl">
+        Take a look at some of the packages on offer, and be sure to read the
+        policy if you're seeking a commission.
+      </p>
+
+      <article className="bg-salmon p-2 mx-1 rounded lg:mx-7 xl:mx-9 xl:mt-7">
+        <h2 className="font-bold text-xl xl:pb-2 bg-white rounded-full w-1/2 m-auto mt-2">
+          Animated Emotes
+        </h2>
+        <img
+          src="images/kami-emotes.jpg"
+          alt="A spread of emotes done for Kamizard on Twitch"
+          className="p-4 m-auto"
+        />
+        <h3 className="mb-2 xl:text-lg font-bold">$50 Each</h3>
+        <hr />
+        <figure className="my-4 px-1">
+          <ul>
+            <li>Description!</li>
+            <li>$15 for seasonal/holiday modifications.</li>
+          </ul>
+        </figure>
+        <hr />
+      </article>
+
       <article className="bg-lorange p-2 mx-1 rounded lg:mx-7 xl:mx-9 xl:mt-7">
         <h2 className="font-bold text-xl xl:pb-2 bg-white rounded-full w-1/2 m-auto mt-2">
           Emotes
@@ -11,9 +35,7 @@ export default function CommissionContent() {
           alt="A spread of emotes done for Kamizard on Twitch"
           className="p-4 m-auto"
         />
-        <h3 className="mb-2 xl:text-lg font-bold">
-          $35 Each
-        </h3>
+        <h3 className="mb-2 xl:text-lg font-bold">$35 Each</h3>
         <hr />
         <figure className="my-4 px-1">
           <ul>
@@ -24,7 +46,30 @@ export default function CommissionContent() {
         <hr />
       </article>
 
-      <article className="bg-salmon p-2 mx-1 rounded lg:mx-7 xl:mx-9">
+      <article className="bg-salmon p-2 mx-1 rounded lg:mx-7 xl:mx-9 xl:mb-8">
+        <h2 className="font-bold text-xl xl:pb-2 bg-white rounded-full w-2/3 m-auto mt-2">
+          Profile Picture / Portrait / Illustration
+        </h2>
+        <img
+          src="images/mateo-smoke.webp"
+          alt="A cute little skeletal t-rex for Tbone"
+          className="m-auto px-4 py-4 xl:px-8 xl:py-4 xl:w-3/4"
+        />
+        <h3 className="mb-2 xl:text-lg font-bold">$50 - $120</h3>
+        <hr />
+        <figure className="my-4 px-1">
+          <ul>
+            <li>*Visual references are required for portraits.</li>
+            <li>
+              Includes 1 custom Profile Picture. 2 Revisions. PNG Files:
+              (800x800)
+            </li>
+          </ul>
+        </figure>
+        <hr />
+      </article>
+
+      <article className="bg-lorange p-2 mx-1 rounded lg:mx-7 xl:mx-9">
         <h2 className="font-bold text-xl xl:pb-2 bg-white rounded-full w-1/2 m-auto mt-2">
           Logo
         </h2>
@@ -45,8 +90,10 @@ export default function CommissionContent() {
         <hr />
       </article>
 
-      <article className="bg-lorange p-2 mx-1 rounded lg:mx-7 xl:mx-9">
-        <h2 className="font-bold text-xl xl:pb-2 bg-white rounded-full w-2/3 m-auto mt-2">Twitch Stream Package</h2>
+      <article className="bg-salmon p-2 mx-1 rounded lg:mx-7 xl:mx-9">
+        <h2 className="font-bold text-xl xl:pb-2 bg-white rounded-full w-2/3 m-auto mt-2">
+          Twitch Stream Package
+        </h2>
         <img
           src="images/niladien-twitch-set.webp"
           alt="A spread of content for Niladien's Twitch stream"
@@ -64,7 +111,7 @@ export default function CommissionContent() {
         <hr />
       </article>
 
-      <article className="bg-salmon p-2 mx-1 rounded lg:mx-7 xl:mx-9">
+      <article className="bg-lorange p-2 mx-1 rounded lg:mx-7 xl:mx-9">
         <h2 className="font-bold text-xl xl:pb-2 bg-white rounded-full w-2/3 m-auto mt-2">
           Subscriber & Bit Badges
         </h2>
@@ -95,18 +142,16 @@ export default function CommissionContent() {
         <hr />
       </article>
 
-      <article className="bg-lorange p-2 mx-1 rounded lg:mx-7 xl:mx-9">
+      <article className="bg-salmon p-2 mx-1 rounded lg:mx-7 xl:mx-9">
         <h2 className="font-bold text-xl xl:pb-2 bg-white rounded-full w-2/3 m-auto mt-2">
           Basic Scenes, Overlays or Banners
         </h2>
-        <img 
-          src="images/des-scenes.webp"  
-          alt="Custom scenes, overlays or banners in the theme of your choice" 
+        <img
+          src="images/des-scenes.webp"
+          alt="Custom scenes, overlays or banners in the theme of your choice"
           className="m-auto p-4"
         />
-        <h3 className="mb-2 xl:text-lg font-bold">
-          $30 Each
-        </h3>
+        <h3 className="mb-2 xl:text-lg font-bold">$30 Each</h3>
         <hr />
         <figure className="my-4 px-1">
           <ul>
@@ -123,8 +168,10 @@ export default function CommissionContent() {
         <hr />
       </article>
 
-      <article className="bg-salmon p-2 mx-1 rounded lg:mx-7 xl:mx-9">
-        <h2 className="font-bold text-xl xl:pb-2 bg-white rounded-full w-1/2 m-auto mt-2">Illustrated Scene</h2>
+      <article className="bg-lorange p-2 mx-1 rounded lg:mx-7 xl:mx-9">
+        <h2 className="font-bold text-xl xl:pb-2 bg-white rounded-full w-1/2 m-auto mt-2">
+          Illustrated Scene
+        </h2>
         <img
           src="images/meeks-starting-soon.jpg"
           alt="A starting soon scene featuring the pokemon blastoise and gengar"
@@ -148,14 +195,19 @@ export default function CommissionContent() {
         <hr />
       </article>
 
-      <article className="bg-lorange p-2 mx-1 rounded lg:mx-7 xl:mx-9">
-        <h2 className="font-bold text-xl xl:pb-2 bg-white rounded-full w-1/2 m-auto mt-2">Basic Panels</h2>
+      <article className="bg-salmon p-2 mx-1 rounded lg:mx-7 xl:mx-9">
+        <h2 className="font-bold text-xl xl:pb-2 bg-white rounded-full w-1/2 m-auto mt-2">
+          Basic Panels
+        </h2>
         <img
           src="images/kestrel-panels.webp"
           alt="Panels and more for DillThrills"
           className="m-auto px-4 py-4 xl:px-8 xl:py-4"
         />
         <h3 className="mb-2 xl:text-lg font-bold">$20 Each</h3>
+        <h3 className="mb-2 xl:text-lg font-bold">
+          Same-style set of 5 for $50
+        </h3>
         <hr />
         <figure className="my-4 px-1">
           <ul>
@@ -170,14 +222,16 @@ export default function CommissionContent() {
         <hr />
       </article>
 
-      <article className="bg-salmon p-2 mx-1 rounded lg:mx-7 xl:mx-9">
-        <h2 className="font-bold text-xl xl:pb-2 bg-white rounded-full w-1/2 m-auto mt-2">Illustrated Panels</h2>
-        <img 
-          src="images/notor-panels.webp" 
+      <article className="bg-lorange p-2 mx-1 rounded lg:mx-7 xl:mx-9">
+        <h2 className="font-bold text-xl xl:pb-2 bg-white rounded-full w-1/2 m-auto mt-2">
+          Illustrated Panels
+        </h2>
+        <img
+          src="images/notor-panels.webp"
           alt="Custom, fully illustrated scene for your streaming channel"
-          className="m-auto p-4" 
+          className="m-auto p-4"
         />
-        <h3 className="mb-2 xl:text-lg font-bold">$30/ea</h3>
+        <h3 className="mb-2 xl:text-lg font-bold">$40/ea</h3>
         <hr />
         <figure className="my-4 px-1">
           <ul>
@@ -195,14 +249,15 @@ export default function CommissionContent() {
         <hr />
       </article>
 
-      <article className="bg-lorange p-2 mx-1 rounded lg:mx-7 xl:mx-9">
+      <article className="bg-salmon p-2 mx-1 rounded lg:mx-7 xl:mx-9">
         <h2 className="font-bold text-xl xl:pb-2 bg-white rounded-full w-2/3 m-auto mt-2">
           Decorative Stream Schedule
         </h2>
-        <img 
-          src="images/DesSchedule.webp" 
-          alt="Wonderfully colourful stream schedules in a variety of styles" 
-          className="m-auto p-4" />
+        <img
+          src="images/DesSchedule.webp"
+          alt="Wonderfully colourful stream schedules in a variety of styles"
+          className="m-auto p-4"
+        />
         <h3 className="mb-2 xl:text-lg font-bold">$40</h3>
         <hr />
         <figure className="my-4 px-1">
@@ -214,38 +269,21 @@ export default function CommissionContent() {
         <hr />
       </article>
 
-      <article className="bg-salmon p-2 mx-1 rounded lg:mx-7 xl:mx-9">
-        <h2 className="font-bold text-xl xl:pb-2 bg-white rounded-full w-1/2 m-auto mt-2">Poster or Graphic</h2>
-        <img src="images/raft-with-friends.webp" alt="Needs Image" className="m-auto p-4" />
+      <article className="bg-lorange p-2 mx-1 rounded lg:mx-7 xl:mx-9">
+        <h2 className="font-bold text-xl xl:pb-2 bg-white rounded-full w-1/2 m-auto mt-2">
+          Poster or Graphic
+        </h2>
+        <img
+          src="images/raft-with-friends.webp"
+          alt="Needs Image"
+          className="m-auto p-4"
+        />
         <h3 className="mb-2 xl:text-lg font-bold">$30</h3>
         <hr />
         <figure className="my-4 px-1">
           <ul>
             <li>Includes: 2 Revisions.</li>
             <li>PNG Files: (1920x1080) or requested size.</li>
-          </ul>
-        </figure>
-        <hr />
-      </article>
-
-      <article className="bg-lorange p-2 mx-1 rounded lg:mx-7 xl:mx-9 xl:mb-8">
-        <h2 className="font-bold text-xl xl:pb-2 bg-white rounded-full w-2/3 m-auto mt-2">
-          Profile Picture / Portrait
-        </h2>
-        <img
-          src="images/mateo-smoke.webp"
-          alt="A cute little skeletal t-rex for Tbone"
-          className="m-auto px-4 py-4 xl:px-8 xl:py-4 xl:w-3/4"
-        />
-        <h3 className="mb-2 xl:text-lg font-bold">$50 - $100</h3>
-        <hr />
-        <figure className="my-4 px-1">
-          <ul>
-            <li>*Visual references are required for portraits.</li>
-            <li>
-              Includes 1 custom Profile Picture. 2 Revisions. PNG Files:
-              (800x800)
-            </li>
           </ul>
         </figure>
         <hr />
