f5cc0881a741ab9cbd7bd7a06753021d0484a68b
Huntthee <mikeallison000@gmail.com>
2023-05-06 13:08:52 -0600

    Added Reactive Images panel to commissions page.

--- pages/components/commissions-content.tsx
+++ pages/components/commissions-content.tsx
@@ -6,10 +6,33 @@ export default function CommissionContent() {
         policy if you're seeking a commission.
       </p>
 
-      <article className="bg-lorange p-2 mx-1 rounded lg:mx-7 xl:mx-9 xl:mt-7">
+      <article className="bg-salmon p-2 mx-1 rounded lg:mx-7 xl:mx-9 xl:mt-7">
         <video autoPlay loop className="xl:p-6">
           <source src="images/emotes_comm_sheets.mp4" type="video/mp4" />
         </video>
+      </article>
+
+      <article className="bg-lorange p-2 mx-1 rounded lg:mx-7 xl:mx-9 xl:mt-7">
+        <h2 className="font-bold text-xl xl:pb-2 bg-white rounded-full w-1/2 m-auto mt-2">
+          Reactive Images
+        </h2>
+        <img
+          src="images/reactive_images.webp"
+          alt="For PNGTubers! Set up these reactive images to respond to your voice input."
+          className="px-4 py-4 xl:px-8 xl:py-4  m-auto"
+        />
+        <h3 className="mb-2 xl:text-lg font-bold">
+          60$
+        </h3>
+        <hr />
+        <figure className="my-4 px-1">
+          <ul>
+            <li className="font-bold">Reactive Images for PNGTubers!</li>
+            <li>4 Images for $60</li>
+            <li>Includes 2 Revisions. *PNG Files</li>
+          </ul>
+        </figure>
+        <hr />
       </article>
 
       <article className="bg-salmon p-2 mx-1 rounded lg:mx-7 xl:mx-9 xl:mt-7">
@@ -68,9 +91,7 @@ export default function CommissionContent() {
           alt="An album cover for Easter x Pluto"
           className="m-auto px-4 py-4 xl:px-8 xl:py-4 "
         />
-        <h3 className="mb-2 xl:text-lg font-bold">
-          $60 
-        </h3>
+        <h3 className="mb-2 xl:text-lg font-bold">$60</h3>
         <hr />
         <figure className="my-4 px-1">
           <ul>
